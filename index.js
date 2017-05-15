d02f3517f95d22cec5b646f5dbf8ee2dd2ee57ac
Elizabeth <elizabethnich19@gmail.com>
2017-05-14 21:55:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//Code React element here
+const list =
+  React.createElement('div', { class: 'me' },
+  React.createElement('h1', {}, 'Liz'),
+  React.createElement('p', {}, "Learnin' some cool stuff"),
+  React.createElement('ul', { class: 'me_interests' },
+  [
+    React.createElement('li', {}, 'Hiking'),
+    React.createElement('li', {}, 'Yoga and smoothies'),
+    React.createElement('li', {}, 'MochiMochiMochi'),
+    React.createElement('li', {}, 'Drawing stick figures in awkward situations')
+  ]
+));
+
+ReactDOM.render(list, document.getElementById('main'));
